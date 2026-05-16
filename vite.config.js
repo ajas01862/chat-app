8e029a10847b1460e90c94e6aaf2864a3debe25a
Vandified <ajas01862@gmail.com>
2026-05-16 20:10:21 +0530

    setup github pages

--- vite.config.js
+++ vite.config.js
@@ -12,13 +12,17 @@
 
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import ghPages from 'vite-plugin-gh-pages'
 
 export default defineConfig(({ mode }) => {
   // Load .env files based on the current mode (e.g., development or production)
   const env = loadEnv(mode, process.cwd());
 
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      ghPages()
+    ],
     optimizeDeps: {
       exclude: ['firebase/auth', 'firebase/app', 'firebase/firestore']
     },
