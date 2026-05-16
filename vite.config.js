f136ee7d0b1faf207e0ac7c4bed9ed2a716c746c
Vandified <ajas01862@gmail.com>
2026-05-16 20:32:03 +0530

    remove broken gh-pages plugin

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@
 
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import ghPages from 'vite-plugin-gh-pages'
+// import ghPages from 'vite-plugin-gh-pages'
 
 export default defineConfig(({ mode }) => {
   // Load .env files based on the current mode (e.g., development or production)
@@ -20,8 +20,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [
-      react(),
-      ghPages()
+      react()
     ],
     optimizeDeps: {
       exclude: ['firebase/auth', 'firebase/app', 'firebase/firestore']
